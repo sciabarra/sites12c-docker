c5299a9819a156c31da3654886abc739967b7f1e
Michele Sciabarra <michele+youtrack@sciabarra.com>
2015-12-23 13:38:07 +0100

    temporary using shared1

--- install-sites/config-sites.sh
+++ install-sites/config-sites.sh
@@ -7,9 +7,10 @@ tail -f admin.log | sed /RUNNING/q
 echo "****" Granting Permissions
 weblogic/user_projects/domains/base_domain/wcsites/bin/grant-opss-permission.sh "$USER" "$PASS"
 cp wcs_properties_bootstrap.ini weblogic/user_projects/domains/base_domain/wcsites/wcsites/config/wcs_properties_bootstrap.ini
-sed -i s/$(cat host.txt)/sites/ weblogic/user_projects/domains/base_domain/wcsites/wcsites/config/jbossTicketCacheReplicationConfig.xml
 echo "****" Starting managed Sites
+sed -i s/$(cat host.txt)/sites/ weblogic/user_projects/domains/base_domain/wcsites/wcsites/config/jbossTicketCacheReplicationConfig.xml
 weblogic/user_projects/domains/base_domain/bin/startManagedWebLogic.sh wcsites_server1 t3://sites:7001 2>&1 >/app/managed.log &
 tail -f managed.log | sed /RUNNING/q
 curl http://$(cat host.txt):7003/sites/HelloCS
 curl http://$(cat host.txt):7003/sites/sitesconfig 
+sed -i s/$(cat host.txt)/sites/ weblogic/user_projects/domains/base_domain/wcsites/wcsites/config/jbossTicketCacheReplicationConfig.xml
