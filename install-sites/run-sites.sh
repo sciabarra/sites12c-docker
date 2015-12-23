f06df9cb19b877e71991ec737219118880267c94
Michele Sciabarra <michele+youtrack@sciabarra.com>
2015-12-23 13:56:14 +0100

    fix again jbossticket.xml

--- install-sites/run-sites.sh
+++ install-sites/run-sites.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+sed -i s/$(cat host.txt)/sites/ weblogic/user_projects/domains/base_domain/wcsites/wcsites/config/jbossTicketCacheReplicationConfig.xml
 weblogic/user_projects/domains/base_domain/bin/startManagedWebLogic.sh wcsites_server1
