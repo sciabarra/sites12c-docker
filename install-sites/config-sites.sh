e67237a27331dad84a0ad62b179749327cb9a848
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-01-04 20:02:01 +0100

    creating the log file before starting

--- install-sites/config-sites.sh
+++ install-sites/config-sites.sh
@@ -2,10 +2,12 @@
 USER=${1:-weblogic}
 PASS=${2:-Welcome1}
 echo "****" Starting weblogic
+touch admin.log
 weblogic/user_projects/domains/base_domain/startWebLogic.sh 2>&1 >/app/admin.log &
 perl tailuntil.pl RUNNING admin.log
 echo "****" Granting Permissions
 weblogic/user_projects/domains/base_domain/wcsites/bin/grant-opss-permission.sh "$USER" "$PASS"
 echo "****" Starting managed Sites
+touch managed.log
 weblogic/user_projects/domains/base_domain/bin/startManagedWebLogic.sh wcsites_server1 t3://sites:7001 2>&1 >/app/managed.log &
 perl tailuntil.pl RUNNING managed.log
