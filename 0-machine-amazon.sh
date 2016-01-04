0d75967167df171b091abdc238423269862a1543
Michele Sciabarra <michele@sciabarra.com>
2016-01-04 16:03:13 +0100

    remove bucket from amazon machine bulder

--- 0-machine-amazon.sh
+++ 0-machine-amazon.sh
@@ -3,14 +3,12 @@ DOCKER_MACHINE_NAME=${1:?machine name}
 AWS_KEY=${2:?amazon access key}
 AWS_SECRET=${3:?amazon secret key}
 AWS_VPC=${4:?amazon vpc id}
-AWS_BUCKET=${5:?s3 bucket}
 MTYPE=t2.large
 #${6:?machine type - recommended t2.large for build, t2.medium to run}
 echo "eval \$(docker-machine env $DOCKER_MACHINE_NAME)"  >_machine.sh
 echo "AWS_KEY=$AWS_KEY">>_machine.sh
 echo "AWS_SECRET=$AWS_SECRET">>_machine.sh
 echo "AWS_VPC=$AWS_VPC">>_machine.sh
-echo "AWS_BUCKET=$AWS_BUCKET">>_machine.sh
 # create machine
 docker-machine create --driver amazonec2 \
 --amazonec2-vpc-id "$AWS_VPC"  \
