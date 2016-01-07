43395a4c7792076da9d6d0f36bba4836f3955521
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-01-07 10:46:15 +0000

    requiring public ip

--- 5-s3load.sh
+++ 5-s3load.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-if test -e _machine.sh
-then
- source $PWD/_machine.sh
- HOST=$(docker-machine ip $DOCKER_MACHINE_NAME)
-else
- HOST=$(hostname -I | awk '{print $1}')
-fi
-EXTRA_HOST="${1:?hostname assigned to sites}:$HOST"
+EXTRA_HOST="${1:?hostname assigned to sites}:${2:?public ip}"
 cp docker-compose.yml.tmpl docker-compose.yml
 echo "    - \"$EXTRA_HOST\"">>docker-compose.yml
-AWS_KEY=${2:?aws key}
-AWS_SECRET=${3?aws secret}
-AWS_BUCKET=${4:?aws bucket name}
+AWS_KEY=${3?aws key}
+AWS_SECRET=${4?aws secret}
+AWS_BUCKET=${5:?aws bucket name}
 source $PWD/registry-on-s3.sh
 docker pull localhost:5000/owcs/sites
 docker pull localhost:5000/owcs/shared
