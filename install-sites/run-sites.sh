37fad168e42cee8530003aa914f0468545d9650f
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-01-21 09:14:09 +0100

    fixes on the sites building, removing a big file from oracle container

--- install-sites/run-sites.sh
+++ install-sites/run-sites.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 sed -i s/$(cat host.txt)/sites/ weblogic/user_projects/domains/base_domain/wcsites/wcsites/config/jbossTicketCacheReplicationConfig.xml
-while ! nc shared.loc 1521 </dev/null 2>/dev/null
-do sleep 1 ; echo waiting for database
-done
-echo wait 15 sec
-sleep 5
-echo wait 10 sec
-sleep 5
-echo wait 5 sec
-sleep 5
+ln -sf /app/weblogic/user_projects/domains/base_domain/servers/wcsites_server1/logs /app/logs
+if test -e /tmp/weblogic-running
+then
+ weblogic/user_projects/domains/base_domain/bin/stopManagedWebLogic.sh wcsites_server1 
+else
+ while ! nc shared.loc 1521 </dev/null 2>/dev/null
+ do sleep 1 ; echo waiting for database
+ done
+ echo wait 15 sec
+ sleep 5
+ echo wait 10 sec
+ sleep 5
+ echo wait 5 sec
+ sleep 5
+ touch /tmp/weblogic-running
+fi
 echo "starting weblogic"
-weblogic/user_projects/domains/base_domain/bin/startManagedWebLogic.sh wcsites_server1
+weblogic/user_projects/domains/base_domain/bin/startManagedWebLogic.sh wcsites_server1 &
